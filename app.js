00da42ef59120022c986dc250a4938e18aca20f4
De Silva Y A S <it23551084@my.sliit.lk>
2026-02-16 14:37:49 +0530

    Add collaboration message to app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 console.log("Hello from Node.js!");
 console.log("------------------------------");
 
+// Collaborating with Sudam(IT23561120) and Me(De Silva Y A S - IT23551084)
+console.log("Collaborating with Sudam(IT23561120) and Me(De Silva Y A S - IT23551084)");
+
 
 const myModule = require('./my_module.js');
 console.log(myModule.myFunction());
